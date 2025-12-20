01b6fe06032402694657e445f9a3584f643fb763
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-12-20 12:28:48 +0100

    Enhance transaction display: add compact transaction partial for improved UI, update transaction model callbacks for broadcasting, and adjust form styling for better layout.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -10,14 +10,17 @@ class Transaction < ApplicationRecord
 
   after_create_commit -> {
     broadcast_prepend_later_to [ user, :transactions ], target: "transactions_list"
+    broadcast_prepend_later_to [ user, :transactions_compact ], partial: "transactions/transaction_compact", target: "transactions_list"
   }
   after_update_commit -> {
     broadcast_replace_later_to [ user, :transactions ]
+    broadcast_replace_later_to [ user, :transactions_compact ], partial: "transactions/transaction_compact"
     broadcast_replace_later_to [ user, :cashflow_summary ], partial: "transactions/cashflow_summary", locals: { user: user }, target: "cashflow_card"
     broadcast_replace_later_to [ user, :cashflow_detail ], partial: "transactions/cashflow_detail", locals: { user: user }, target: "cashflow_card"
   }
   after_destroy_commit -> {
     broadcast_remove_to [ user, :transactions ]
+    broadcast_remove_to [ user, :transactions_compact ]
     broadcast_replace_to [ user, :cashflow_summary ], partial: "transactions/cashflow_summary", locals: { user: user }, target: "cashflow_card"
     broadcast_replace_to [ user, :cashflow_detail ], partial: "transactions/cashflow_detail", locals: { user: user }, target: "cashflow_card"
   }
