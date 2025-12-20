40ab443594006b17654d3c0b668bcc082c62be6d
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-12-20 15:56:22 +0100

    Add month selection for cashflow detail: implement dropdown for selecting months in stats view, update controller to handle month parameter, and adjust cashflow detail rendering accordingly.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,5 +2,7 @@ class StatsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @months = params[:months]&.to_i || 6
+    @months = [ 3, 6, 12, 18, 24 ].include?(@months) ? @months : 6
   end
 end
