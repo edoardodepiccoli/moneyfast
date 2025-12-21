cd9673c8b8611fe4b9117b2ff1e2879e34e6dfc5
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-12-21 13:48:56 +0100

    Add turbo progress bar: implement a fixed progress bar in the application layout and configure immediate display in JavaScript for improved user feedback during navigation.

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
 
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.setProgressBarDelay(0)  // show immediately
+
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
